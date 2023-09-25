db67051de86ab0657bb628cd8e1295be8d0225af
isa-ismail <fahimismail75.scc@gmail.com>
2023-09-25 22:15:57 +0600

    pg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,12 @@ import Link from 'next/link';
 
 
 export default function Home() {
+  
   return (
     <div className='min-h-screen grainy bg-gradient-to-r from-rose-100 to-teal-100'>
       <div className='min-h-screen flex flex-col items-center justify-center'>
         <h1 className='font-semibold text-7xl text-center'>
           AI{' '}
-          
           <span className='text-green-600 font-bold'>
             note taking
           </span>
