983745674273017770eeed8728e5ad09f4cdfd88
isa-ismail <fahimismail75.scc@gmail.com>
2023-09-26 22:28:22 +0600

    love

--- src/app/api/createNoteBook/route.ts
+++ src/app/api/createNoteBook/route.ts
@@ -3,10 +3,12 @@ import { auth } from "@clerk/nextjs"
 import { NextResponse } from "next/server"
 
 export async function POST(req: Request) {
+
   // const { userId } = auth()
   // if (!userId) {
   //   return new NextResponse('Unauthorized', { status: 401 })
   // }
+  
   const { body } = await req.json()
   const { name } = body
   const image_description = await generateImagePrompt(name)
