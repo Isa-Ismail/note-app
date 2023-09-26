4a46aec7121abee10415ef319fe3094acef0b935
isa-ismail <fahimismail75.scc@gmail.com>
2023-09-26 21:37:36 +0600

    api

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,7 @@
+import { auth } from "@clerk/nextjs"
+import { NextResponse } from "next/server"
+
+export async function GET(req: Request) {
+  
+  return new NextResponse(JSON.stringify({ hello: 'world' }))
+}
