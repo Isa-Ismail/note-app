768ce832bd6737141877c966eac5cb6b7cea2941
isa-ismail <fahimismail75.scc@gmail.com>
2023-09-26 22:58:02 +0600

    hello

--- src/app/api/createNoteBook/route.ts
+++ src/app/api/createNoteBook/route.ts
@@ -1,17 +1,6 @@
-// import { generateImagePrompt } from "@/lib/openai"
-// import { auth } from "@clerk/nextjs"
 import { NextResponse } from "next/server"
 
 export async function GET(req: Request) {
-
-  // const { userId } = auth()
-  // if (!userId) {
-  //   return new NextResponse('Unauthorized', { status: 401 })
-  // }
-
-  // const { body } = await req.json()
-  // const { name } = body
-  // const image_description = await generateImagePrompt(name)
-  // console.log(image_description)
-  return new NextResponse(JSON.stringify({ image_description: 'hi' }))
+  
+  return new NextResponse(JSON.stringify({ hello: 'world' }))
 }
